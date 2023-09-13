3dfca30cf62fd9654404911bc16debc8b60962b3
irena_weigand <irena.weigand@gmail.com>
2023-09-13 21:44:04 +0200

    inicijalno

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Component } from 'react'
+import './App.css'
+
+class App extends Component {
+  render(){
+    return (
+        <>
+          <h1>State</h1>
+        </>
+    )
+  }
+  
+}
+
+export default App
