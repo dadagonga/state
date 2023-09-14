c8829abb427ccf06962bb943dca7aa7ae547407b
irena_weigand <irena.weigand@gmail.com>
2023-09-14 17:53:16 +0200

    dz

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,38 @@ const users = [
 ] 
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       users: [
+        {name: 'Marko', years:35},
+        {name: 'Ivan', years:30},
+        {name: 'Ana', years:25},
+       ],
+       tekst: "Proizvoljan tekst"
+    }
+  }
+
+  uvecajGodine = () => {
+    const newUsers = this.state.users.map (user => {
+      return {...user, years: user.years + 1 }
+    })
+    this.setState({users: newUsers})
+  }
+
   render(){
+    const {users, tekst} = this.state
     return (
         <>
           <h1>State</h1>
           < UserClass name={users[0].name} years={users[0].years}/>
           < UserFunction name={users[1].name} years={users[1].years}/>
           < UserChildren name={users[2].name} years={users[2].years}>
-          a hobi mi je programiranje...
+            a hobi mi je programiranje...
+          <p>{tekst}</p>
           </UserChildren>
-
+          <button onClick={this.uvecajGodine}> Uvećaj godine</button>
         </>
     )
   }
